6af4644ad693ce6f227c77df4b6de6e7f2d05a7b
rezha <rezha.bahari@gmail.com>
2024-03-07 11:02:50 +0700

    adding nav with sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      {/* main nav */}
+      <Sidebar />
+
+      {/* mobile nav */}
+
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
